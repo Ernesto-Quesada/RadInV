60dc5e8a7cdfef879cf0953dbfe78217664c099a
ernestques <ernesqr@yahoo.com>
2017-12-03 22:16:48 -0500

    fixing

--- RadBack/radinv/app.js
+++ RadBack/radinv/app.js
@@ -38,7 +38,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(layouts);
 app.use(session({
-  secret: 'mymoneysender',
+  secret: 'radiationcount',
   resave:true,
   saveUninitialized:true,
 }));
